7942945c7a79cd739cbeacbf6248bf105980516e
Felipe Nasciemnto <Feliph08@hotmail.com>
2024-12-10 02:41:21 -0300

    aplicando feliph2004.pythonanywhere.com no allowed_hosts

--- nowon/settings.py
+++ nowon/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-#cm8^!rb3vgrqcw!q5d565^g(9@34yq_ccoxdv7p1i6sr*7)av
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['feliph2004.pythonanywhere.com', 'thefeliph.pythonanywhere.com', 'thefeliph.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['feliph2004.pythonanywhere.com', '127.0.0.1', 'localhost']
+
 
 
 
